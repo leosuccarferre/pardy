cdff342e3a99e52110f4bd06bbc1e50a44be050c
Leo Succar-Ferré <leosuccarferre@gmail.com>
2019-02-13 22:20:13 -0800

    scaffolding stack

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -69,7 +69,7 @@ export default function Nav() {
               large
               type="email"
               leftIcon="envelope"
-              autoFocus="true"
+              autoFocus={true}
             />
           </FormGroup>
 
